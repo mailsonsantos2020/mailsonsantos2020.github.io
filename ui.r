9f7a9140e4eddf3b0f820ccad4a3277eb667fade
mailsonsantos2020 <mailson.melo.santos@gmail.com>
2020-07-30 17:48:39 -0300

    commit 1

--- ui.r
+++ ui.r
@@ -1,25 +1,42 @@
-shinyUI(bootstrapPage(
+library(shiny)
 
-  selectInput(inputId = "n_breaks",
-              label = "Number of bins in histogram (approximate):",
-              choices = c(10, 20, 35, 50),
-              selected = 20),
-
-  checkboxInput(inputId = "individual_obs",
-                label = strong("Show individual observations"),
-                value = FALSE),
-
-  checkboxInput(inputId = "density",
-                label = strong("Show density estimate"),
-                value = FALSE),
-
-  plotOutput(outputId = "main_plot", height = "300px"),
-
-  # Display this only if the density is shown
-  conditionalPanel(condition = "input.density == true",
-    sliderInput(inputId = "bw_adjust",
-                label = "Bandwidth adjustment:",
-                min = 0.2, max = 2, value = 1, step = 0.2)
-  )
-
-))
+ui <- fluidPage(
+    sidebarLayout(
+        sidebarPanel(
+            width = 3,
+            h2("My Static website"),
+            h3("Internal link example"),
+            a(h4("Birds"),
+              href = "#birds"),
+            hr(),
+            
+            h3("External link example"),
+            a(h4("Wikipedia"),
+              href = "https://en.wikipedia.org/wiki/Main_Page",
+              target = "blank")
+        ),
+        
+        mainPanel(
+            width = 9,
+            
+            h2("Link to a saved sample.html"),
+            p("The url is https://johndoe.github.io/samples/sample.html"),
+            a(h3("Lovely Birds"),
+              href = "https://johndoe.github.io/samples/sample.html",
+              target = "blank"),
+            hr(),
+            
+            h2("Text and image example", id = "birds"),
+            fluidRow(
+                column(
+                    7,
+                    includeMarkdown("markdown/birds.Rmd")
+                ),
+                column(
+                    5,
+                    img(src = "birds.png", width = "100%")
+                )
+            ),
+        )
+    )
+)
