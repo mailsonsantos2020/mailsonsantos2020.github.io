f48fdf34e8f6af40c07f726d36d5d17d9436b405
mailsonsantos2020 <mailson.melo.santos@gmail.com>
2020-08-11 12:01:36 -0300

    Teste UI

--- server.r
+++ server.r
@@ -1,29 +1,4 @@
-##################################
-# Website my github              #
-# by Mailson Santos              #
-# app.R file                     #
-##################################
-
 library(shiny)
-library(shinydashboard)
-library(shinycssloaders)
-
-###############
-# LOAD SERVER # 
-###############
-
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-    
-  
-  ## Download PDF
-  output$downloadData <- downloadHandler(
-    filename = "your-pdf-name.pdf",
-    content = function(file) {
-      file.copy("www/teste.pdf", file)
-    }
-  )
-
-  
-  
+    # leave empty
 }
